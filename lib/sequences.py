d5457a2e719abb8b649dc2b19ab66cb821c1534f
=SarahMumbua <mumbuasarah2030@gmail.com>
2023-05-27 10:16:18 +0300

    Completed Assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,12 @@
-#!/usr/bin/env python3
+# sequences.py
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print([])
+    elif length == 1:
+        print([0])
+    else:
+        fib_sequence = [0, 1]
+        for i in range(2, length):
+            fib_sequence.append(fib_sequence[i - 1] + fib_sequence[i - 2])
+        print(fib_sequence)
